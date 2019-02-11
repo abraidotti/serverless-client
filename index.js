83084b45119f96b6cc8b711f0c5473ea5d5e2b78
abraidotti <alessandro.braidotti@gmail.com>
2019-02-11 15:11:57 -0500

    fix eslint issue and syntax settings

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const request = require('request');
-const schedule = require('node-schedule');
+const request = require('request')
+const morgan = require('morgan')
+const schedule = require('node-schedule')
 
-const machineId = '1';
+const machineId = '1'
 
-console.log('Client is running.');
+console.log('Client is running.')
 
 schedule.scheduleJob('*/5 * * * *', () => {
-  const date = new Date();
+    const date = new Date()
 
-  request.post('https://oagfppjnva.execute-api.us-east-1.amazonaws.com/dev/ping', {
-    json: {
-      machineId,
-      time: date,
-    },
-  }, (error, res, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    console.log(`statusCode: ${res.statusCode}`);
-    console.log(body);
-  });
-});
+    request.post('https://oagfppjnva.execute-api.us-east-1.amazonaws.com/dev/ping', {
+        json: {
+            machineId,
+            time: date
+        }
+    }, (error, res, body) => {
+        if (error) {
+            console.error(error)
+            return
+        }
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+    })
+})
